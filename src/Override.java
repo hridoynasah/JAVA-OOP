86a7ad08cd64dfda7221cbf5c0cbee2496cb2830
Hridoy <hridoynasah@gmail.com>
2024-07-30 20:46:11 +0600

    First Commit

--- src/Override.java
+++ src/Override.java
@@ -1,2 +1,13 @@
+// If you don't want override use final keyword :
+// Error after using final keyword - java: cannot assign a value to final variable x
+
 public class Override {
+    final int x = 10;
+
+    public static void main(String[] args){
+        Override MyObj = new Override();
+        System.out.println(MyObj.x);
+//        MyObj.x = 100;
+//        System.out.println(MyObj.x);
+    }
 }
